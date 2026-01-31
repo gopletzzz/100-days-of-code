e0947424a43ae50b7dfc0b2f961360c3ec98baa5
ramadhan rafli pratama <gopletzzz@gmail.com>
2026-02-01 06:37:06 +0700

    update

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,1 +1,7 @@
-print("Hello" + " Angela")
+
+print("Welcome to the Band Name Generator")
+
+city =  input("what is the name of the city you grew up in?\n")
+pet = input("Whats your pet's name? \n")
+
+print("your band could be " + city +" "+ pet)
